f51215e41dc84f1f8bb35756f9abba1b02352a41
Robert Quintana <robertquintanatx@gmail.com>
2020-06-07 06:46:12 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Dog 
+  def name=(chosen_name)
+    @doggos_name = chosen_name
+  end 
+  
+  def name
+    @doggos_name
+  end 
+  
+  def bark
+    puts "woof!"
+  end 
+end 
